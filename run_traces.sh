37202712c95321734e9ca0faea6bdfab816cb5a3
paavanindela <190050051@iitb.ac.in>
2021-11-27 16:32:14 +0530

    update run_traces

--- run_traces.sh
+++ run_traces.sh
@@ -1,11 +1,40 @@
 #!/bin/bash
 
 ./build_champsim.sh bimodal no ipcp ipcp ipcp lru 1 
-
+./build_champsim.sh bimodal no ipcp+bingo ipcp ipcp lru 1
+./build_champsim.sh bimodal no ipcp+sangam ipcp ipcp lru 1
+./build_champsim.sh bimodal no ipcp+pangloss ipcp ipcp lru 1
+./build_champsim.sh bimodal no ipcp+t_skid ipcp ipcp lru 1
+./build_champsim.sh bimodal no ipcp+mlop ipcp ipcp lru 1
 echo "TRACES"
 while read LINE
 do
     ./run_champsim.sh bimodal-no-ipcp-ipcp-ipcp-lru-1core 10 10 $LINE &
+done < ./scripts/dpc3_max_simpoint.txt
+wait
+while read LINE
+do
+    ./run_champsim.sh bimodal-no-ipcp+bingo-ipcp-ipcp-lru-1core 10 10 $LINE &
+done < ./scripts/dpc3_max_simpoint.txt
+wait
+while read LINE
+do
+    ./run_champsim.sh bimodal-no-ipcp+sangam-ipcp-ipcp-lru-1core 10 10 $LINE &
+done < ./scripts/dpc3_max_simpoint.txt
+wait
+while read LINE
+do
+    ./run_champsim.sh bimodal-no-ipcp+pangloss-ipcp-ipcp-lru-1core 10 10 $LINE &
+done < ./scripts/dpc3_max_simpoint.txt
+wait
+while read LINE
+do
+    ./run_champsim.sh bimodal-no-ipcp+t_skid-ipcp-ipcp-lru-1core 10 10 $LINE &
+done < ./scripts/dpc3_max_simpoint.txt
+wait
+while read LINE
+do
+    ./run_champsim.sh bimodal-no-ipcp+mlop-ipcp-ipcp-lru-1core 10 10 $LINE &
 done < ./scripts/dpc3_max_simpoint.txt
 wait
 echo "DONE"
